c95918336a056a943fc0f3a247a05fb00c1d5a93
Aman <amancheema911@gmail.com>
2026-02-11 10:16:03 +0530

    update the url and smooth scroll

--- .next/dev/static/chunks/_d9e15fab._.js
+++ .next/dev/static/chunks/_d9e15fab._.js
@@ -13,7 +13,7 @@ __turbopack_context__.s([
     ()=>socialLinks
 ]);
 const APP_NAME = "GetSetTime";
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = "https://getsettime.com/";
 const contactInfo = {
     email: "office.getsettime@gmail.com",
     address: "Chandigarh, India"
