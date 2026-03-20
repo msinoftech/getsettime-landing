fdf92a08159af068af7f6815954b7f0659fa9550
Aman <amancheema911@gmail.com>
2026-03-20 11:26:16 +0530

    update the robots and url

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "",
-      disallow: ["api", "privacy-policy", "terms-of-conditions"],
+      disallow: ["api"],
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
     host: BASE_URL,
